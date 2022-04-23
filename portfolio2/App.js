1066e5963b559a8ea9528c86012d3aef5881e09f
Emily Foley <efoley0823@gmail.com>
2022-04-23 17:32:13 -0400

    April 23, 2022 5:32 PM

--- portfolio2/App.js
+++ portfolio2/App.js
@@ -25,9 +25,9 @@ function Warmups(){
     },
     { 
       id: 3,  
-      name: "T-shirt",
+      name: "Butterfly stretch",
       image: 'https://images.unsplash.com/photo-1622470953794-aa9c70b0fb9d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
-      description: "Oversized graphic tee",
+      description: "Sit on the floor or a prop with the soles of your feet pressing into each other.",
     },
   ]
   const renderData = (item) => {
