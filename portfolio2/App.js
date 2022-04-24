964247186420d9291993e59bcf43e52c73b9effd
Emily Foley <efoley0823@gmail.com>
2022-04-23 23:00:56 -0400

    April 23, 2022 11:00 PM

--- portfolio2/App.js
+++ portfolio2/App.js
@@ -10,25 +10,47 @@ import {Card} from 'react-native-paper';
 const Stack = createNativeStackNavigator();
 
 function Cooldown() {
-  const [seconds, setSeconds] = useState(30);
-const [pause, setPause] = useState(false);
+  const [seconds, setSeconds] = useState(0);
+  const [isActive, setIsActive] = useState(false);
 
-useEffect(() => {
-  const interval = setInterval(() => {
-    if(!pause) { //I used '!paused' because I set pause initially to false. 
-      if (seconds > 0) {
-        setSeconds(seconds - 1);
-      }
+  function toggle() {
+    setIsActive(!isActive);
+  }
+
+  function reset() {
+    setSeconds(0);
+    setIsActive(false);
+  }
+
+  useEffect(() => {
+    let interval = null;
+    if (isActive) {
+      interval = setInterval(() => {
+        setSeconds(seconds => seconds + 1);
+      }, 1000);
+    } else if (!isActive && seconds !== 0) {
+      clearInterval(interval);
     }
-  }, 1000);
-  return () => clearInterval(interval);
-});
+    return () => clearInterval(interval);
+  }, [isActive, seconds]);
 
-const handlePauseToggle = () => {
-  setPause(!pause);
-}
+  return (
+    <div className="app">
+      <div className="time">
+        {seconds}s
+      </div>
+      <div className="row">
+        <button className={`button button-primary button-primary-${isActive ? 'active' : 'inactive'}`} onClick={toggle}>
+          {isActive ? 'Pause' : 'Start'}
+        </button>
+        <button className="button" onClick={reset}>
+          Reset
+        </button>
+      </div>
+    </div>
+  );
+};
 
-}
 
 
 function Warmups(){
